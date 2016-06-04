c8168f775ccdc3e0868ff3e058bbd455296360cb
andfroswe <gummianka88@gmail.com>
2016-06-04 16:36:08 +0200

    debouncing does not fully work, might have to change this in the future. the front pedal now has the functionality it needs

--- stage_pedal/stage_pedal.ino
+++ stage_pedal/stage_pedal.ino
@@ -1,28 +1,9 @@
-#define DEBOUNCE_TIME_MS 100
-volatile int int_pin = 0;
+#define INTERRUPT_HOLD_TIME_MS 500
 
-ISR(PCINT0_vect) {
-  /* interrupt fired on digital pin 8-13 */
-  PCMSK0 = 0x00;  // disable pin interrupts
-  if (PINB & (1 << PB1)) {
-    int_pin = 9;
-  } else if (PINB & (1 << PB2)) {
-    int_pin = 10;
-  } else {
-    int_pin = 100;
-  }
-  Serial.print("Interrupt on: ");
-  Serial.println(int_pin);
-  int_pin = 0;  // pin sense reset
-  start_timer();  // timer to protect from too frequent switches
-}
+volatile int send_message = 0;  // if 1, message is sent over serial
+volatile int int_pin = 0;     // pin that was changed
 
-ISR(TIMER1_COMPA_vect) {
-  /* timer interrupt */
-  enable_pin_interrupts();
-  TIMSK1 &= ~(1 << OCIE1A);  // stop timer
-}
-
+/* pin interrupts */
 void init_pin_interrupts() {
   cli();  // disable interrupts
   PCICR |= (1 << PCIE0);  // enable interrupts on PC0
@@ -33,13 +14,28 @@ void enable_pin_interrupts() {
   PCMSK0 |= (1 << PCINT1) | (1 << PCINT2); // enable interrupts
 }
 
+ISR(PCINT0_vect) {
+  /* interrupt fired on digital pin 8-13 change */
+  int pinn = PINB;
+  PCMSK0 = 0x00;  // disable pin interrupts
+  /* sense which pin was triggered */
+  if (pinn & (1 << PB1)) {
+    int_pin = 9;
+    send_message = 1;
+  } else if (pinn & (1 << PB2)) {
+    int_pin = 10;
+    send_message = 1;
+  }
+  start_timer();  // timer to protect from too frequent switches
+}
+/* timer interrupts */
 void init_timer() {
   /* setup 16 bit counter timer */
   cli();  // disable interrupts
   TCCR1A = 0x00;  // reset register
   TCCR1B = 0x00;  // reset register
   //TCNT1 = 0;    // reset counter
-  OCR1A = 15624 * 3; // set counter top value
+  OCR1A = 16 * INTERRUPT_HOLD_TIME_MS; // set time before interrupt
   TCCR1B |= (1 << WGM12); // set ctc mode
   TCCR1B |= (1 << CS12) | (1 << CS10); // set clock source
   //TIMSK1 |= (1 << OCIE1A);  // enable compare match on OCRA
@@ -47,15 +43,23 @@ void init_timer() {
 }
 
 void start_timer() {
+  /* start a new timer */
   TCNT1 = 0;  // reset counter
   TIMSK1 |= (1 << OCIE1A);  // start timer in CTC mode
 }
 
+ISR(TIMER1_COMPA_vect) {
+  /* timer interrupt starts pin interrupts again*/
+  enable_pin_interrupts();
+  TIMSK1 &= ~(1 << OCIE1A);  // stop timer
+}
 
+/* main scripts */
 void setup() {
   /* pin setups */
   pinMode(9, INPUT);
   pinMode(10, INPUT);
+  /* initiate functions */
   init_timer();
   init_pin_interrupts();
   enable_pin_interrupts();
@@ -63,4 +67,15 @@ void setup() {
 }
 
 void loop() {
+  /* constantly checks to see if a pin has been triggered and
+   *  sends a message if it has.
+   */
+  if (send_message) {
+    /* send information via serial */
+    Serial.print("Interrupt on: ");
+    Serial.println(int_pin);
+    int_pin = 0;  // reset read pin variable
+    send_message = 0; // reset send message command
+  }
+  //delay(100);
 }

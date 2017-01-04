cd42e81b6b87dc5fff8b8e13d23224f92f4710b8
andfroswe <gummianka88@gmail.com>
2017-01-04 14:57:13 +0100

    Made a state machine for main unit. Save or exit hierarchical state works

--- Software/MasterControlUnit/MasterControlUnit/main.c
+++ Software/MasterControlUnit/MasterControlUnit/main.c
@@ -7,8 +7,45 @@
 
  #include "main.h"
 
- /* Globals */
+ /*** Globals ***/
 volatile long enc_count = 0; /* Global that hold current value for encoder count */
+
+/** State machine variables **/
+/* Function pointer array and enum must match! */
+void (*states[]) (void) = {s_save, s_cancel, s_idle}; /* Function pointer array containing event entry actions */
+enum state_codes {save, cancel, idle}; /* State variables */
+
+/* Events */
+enum events {e_enc_plus, e_enc_minus, e_enc_button}; /* System events */
+
+/* Transitions */
+struct transistions {
+	/* Struct used in transition lookup */
+	enum state_codes this_state; /* Current state */
+	enum events this_event; /* Current event */
+	enum state_codes next_state; /* Next event based on lookup */
+};
+
+/* Transition lookup */
+struct transistions state_transitions [] = {
+	/* Array that determines what happens during an event */
+	{save, e_enc_plus, cancel},
+	{save, e_enc_minus, cancel},
+	{save, e_enc_button, idle},
+	{cancel, e_enc_plus, save},
+	{cancel, e_enc_minus, save},
+	{cancel, e_enc_button, idle},
+	{idle, e_enc_plus, idle},
+	{idle, e_enc_minus, idle},
+	{idle, e_enc_button, save}
+};
+
+/* State machine defines */
+#define TRANS_COUNT (sizeof(state_transitions)/sizeof(*state_transitions)) /* Define number of transitions */
+#define ENTRY_STATE idle /* Starting state */
+/* State machine globals */
+enum state_codes cur_state; /* Global containing current state */
+void (*state_fun) (void); /* Global state function pointer */
 
 int main(void)
 {
@@ -18,18 +55,17 @@ int main(void)
 	setup_debounce();
 	DDRC &= ~_BV(PC0) & ~_BV(PC1);
 	PORTC = _BV(PC0) | _BV(PC1);
+	/* Setup state machine */
+	cur_state = ENTRY_STATE; /* Define starting state */
+	state_fun = states[cur_state]; /* Get first state function */
+	state_fun(); /* Execute first state function */
+
     while (1) 
     {
-	/*
-		if(!(PINC & _BV(PC0))) {
-			SET(ENC_LED_PORT, ENC_GREEN);
-		} else {
-			CLEAR(ENC_LED_PORT, ENC_GREEN);
-		}
-		*/
     }
 }
 
+/** Setup functions **/
 void enc_setup_interrupts(void)
 {
 	/* 
@@ -40,7 +76,6 @@ void enc_setup_interrupts(void)
 	PCMSK2 = _BV(ENC_A_PIN) | _BV(ENC_B_PIN); /* Mask desired pins */
 	sei(); /* Enable interrupts */
 }
-
 void enc_setup_pins(void)
 {
 	/* 
@@ -56,7 +91,6 @@ void enc_setup_pins(void)
 	CLEAR(ENC_LED_PORT, ENC_GREEN);
 	CLEAR(ENC_LED_PORT, ENC_RED);
 }
-
 void setup_debounce(void)
 {
 	/* 
@@ -72,7 +106,24 @@ void setup_debounce(void)
 	TIMSK0 |= _BV(OCIE0A); /* Enable timer interrupt */
 	sei(); /* Enable interrupts */
 }
+/** Encoder leds **/
+void enc_only_green(void)
+{
+	SET(ENC_LED_PORT, ENC_GREEN); /* Light green led */
+	CLEAR(ENC_LED_PORT, ENC_RED); /* Turn off red led */
+}
+void enc_only_red(void)
+{
+	SET(ENC_LED_PORT, ENC_RED); /* Light red led */
+	CLEAR(ENC_LED_PORT, ENC_GREEN); /* Turn off green led */
+}
+void enc_no_led(void)
+{
+	CLEAR(ENC_LED_PORT, ENC_RED); /* Turn off red led */
+	CLEAR(ENC_LED_PORT, ENC_GREEN); /* Turn off green led */
+}
 
+/** Interrupt service routines **/
 ISR(ENC_INT_VECTOR) 
 {
 	/* 
@@ -81,14 +132,29 @@ ISR(ENC_INT_VECTOR)
 	 */
 	static int8_t lookup_table[] = {0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0};
 	static uint8_t enc_val = 0;
+	static int8_t enc_count = 0;
 
 	enc_val = enc_val << 2;    /* Make room for new readings */
 	/* This part will need to be changed if encoder pins are changed */
 	enc_val = enc_val | ((PIND & 0x0C) >> 2); /* Mask encoder pins and move them to LSB */
+	enc_count += lookup_table[enc_val & 0x0F];  // update encoder history
 
-	enc_count += lookup_table[enc_val & 0x0F];  /* Update encoder history */
+	/* State machine handling */
+	enum events cur_event;
+	if ((enc_count/4) == 1) { /* Enc_plus event */
+		cur_event = e_enc_plus;
+		cur_state = lookup_transition(cur_state, cur_event); /* Find next state */
+		state_fun = states[cur_state]; /* Get state function */
+		state_fun(); /* Execute state function */
+		enc_count = 0;
+	} else if ((enc_count/4) == -1) {
+		cur_event = e_enc_minus;
+		cur_state = lookup_transition(cur_state, cur_event); /* Find next state */
+		state_fun = states[cur_state]; /* Get state function */
+		state_fun(); /* Execute state function */
+		enc_count = 0;
+	}
 }
-
 ISR(TIMER0_COMPA_vect)
 {
 	/* 
@@ -103,6 +169,35 @@ ISR(TIMER0_COMPA_vect)
 	State = (State<<1) | ((PINC & _BV(PC0)) >> PC0) | 0xe000;
 	if (State == 0xf000) { /* State == state comparison */
 		/* Action if button is pressed */
-		TOGGLE(ENC_LED_PORT, ENC_GREEN);
+		cur_state = lookup_transition(cur_state, e_enc_button); /* Find next state */
+		state_fun = states[cur_state]; /* Get state function */
+		state_fun(); /* Execute state function */
 		}
 }
+
+/** State functions **/
+void s_save(void)
+{
+	enc_only_green();
+}
+void s_cancel(void)
+{
+	enc_only_red();
+}
+void s_idle(void)
+{
+	enc_no_led();
+}
+/* State support functions */
+enum state_codes lookup_transition(enum state_codes s, enum events e)
+{
+	int i; /* Loop variable */
+	for(i = 0; i < TRANS_COUNT; ++i)
+	{
+		if ((s == state_transitions[i].this_state) && (e == state_transitions[i].this_event)) /* Find state and event in lookup */
+		{
+			return state_transitions[i].next_state; /* Return the next state */
+		}
+	}
+};
+
--- Software/MasterControlUnit/MasterControlUnit/main.h
+++ Software/MasterControlUnit/MasterControlUnit/main.h
@@ -12,11 +12,11 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-/* Includes */
+/*** Includes ***/
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-/* Defines */
+/*** Defines ***/
 #define ENC_INT_VECTOR PCINT2_vect /* Interrupt vector for encoder pulses */
 #define ENC_REGISTER DDRD /* Direction register of encoder pulses */
 #define ENC_PORT PORTD /* Port register of encoder */
@@ -31,14 +31,31 @@
 #define PRESCALER 1024 /* Prescaler used in debouncing */
 #define OCNT_MAX F_CLK/PRESCALER*DEBOUNCE_MS/1000-1 /* Compare counter max value before interrupt is generated */
 
-/* Macros */
+/*** Macros ***/
+/* Bit changing macros */
 #define SET(P, B) P |= _BV(B) /* Macro for setting register B on port P */
 #define CLEAR(P, B) P &= ~_BV(B) /* Macro for clearing bit B on port P */
 #define TOGGLE(P, B) P ^= _BV(B) /* Macro for toggling bt B on port P */
 
-/* Prototypes */
+/*** Prototypes ***/
+/** Encoder **/
 void enc_setup_pins(void); /* Setup i/o */
 void enc_setup_interrupts(void); /* Setup pin interrupts for encoder */
 void setup_debounce(void); /* Setup button debouncing */
+void enc_only_green(void); /* Turn on green led and turn off red */
+void enc_only_red(void); /* Turn on red led and turn off green */
+void enc_no_led(void); /* Turn off both leds */
+
+/**  State machine **/
+/* Declarations */
+enum events;
+enum state_codes;
+struct transitions;
+/* These are the states that determine the behaviour of the system */
+void s_save(void); /* Save changes */
+void s_cancel(void); /* Discard changes */
+void s_idle(void); /* Temporary state */
+enum state_codes lookup_transition(enum state_codes, enum events);
+
 
 #endif /* MAIN_H_ */

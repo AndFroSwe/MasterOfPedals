66c7cea8c883a1c6de21ca157029ad2ef2364942
andfroswe <gummianka88@gmail.com>
2017-01-27 14:31:35 +0100

    Can now read button

--- Software/Prototype/prototype.c
+++ Software/Prototype/prototype.c
@@ -19,8 +19,8 @@ pedalstates preset1[PEDAL_COUNT] = {ON, ON, OFF};
 
 int main(int argc, char *argv[])
 {
-    changePreset(preset1);
-
+    setupTimerInterrupt();
+    while(1){}
     return 0;
 }
 
@@ -45,4 +45,31 @@ void changePreset(pedalstates *preset)
     } else if (preset[1] == OFF) {
         PORT_OFF(P3_PORT, P3);
     }
+}
+
+inline void buttonRead(void)
+{
+    static uint16_t read_mask = 0xe000; /* Masks out unwanted values */
+    static uint16_t state = 0x00; /* Pin history */
+    /* Shift | read button | mask away ignored values */
+    state = (state << 1) | (BUT_REG & _BV(BUT)) >> BUT | (read_mask << 1);
+    if (state == read_mask) {
+        changePreset(preset1);
+    }
+}
+
+void setupTimerInterrupt (void) 
+{
+    /* Starts a timer interrupt that triggers the button read */
+	cli(); /* Disable interrupts */
+	TCCR0A |= _BV(WGM01); /* Set to CTC mode */
+	TCCR0B |= _BV(CS02) | _BV(CS00); /* Set clock source and prescaler */
+	OCR0A =	OCNT_MAX; /* Set max counter value */
+	TIMSK0 |= _BV(OCIE0A); /* Enable timer interrupt */
+	sei(); /* Enable interrupts */
+}
+
+ISR(TIMER0_COMPA_vect)
+{
+    buttonRead();
 }
--- Software/Prototype/prototype.h
+++ Software/Prototype/prototype.h
@@ -17,16 +17,25 @@
 #define P2 PC4
 #define P3_PORT PORTC
 #define P3 PC5
-
+/* Button debounce */
+#define BUT_REG PORTD /* Button register */
+#define BUT PD1 /* Button pin */
+#define DEBOUNCE_MS 5 /* ms between button reads */
+#define F_CLK 8000000UL /* Internal clock frequency [Hz] */
+#define PRESCALER 1024 /* Prescaler used in debouncing */
+#define OCNT_MAX F_CLK/PRESCALER*DEBOUNCE_MS/1000-1 /* Compare counter max value before interrupt is generated */
 
 /* Includes */
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 /* Typedefs */
 typedef enum pedalstates {ON, OFF} pedalstates;
 
 /* Function prototypes */
 void changePreset(pedalstates *); /* Engages pedals according to preset */
+inline void buttonRead(void); /* Debounced read of a button */
+void setupTimerInterrupt(void); /* Setup timer interrupt used in button db*/
 
 
 #endif /* ifndef PROTOTYPE_H */
